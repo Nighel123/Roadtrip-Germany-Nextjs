755114bb1a502b877cb398fd129afd5f3db8d5b6
Nighel123 <nickel.paulsen@gmail.com>
2024-08-21 20:15:32 +0200

    all images disappeard on website

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,8 @@ export default NextAuth(authConfig).auth;
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ["/((?!api|_next/static|_next/image|images).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|images|.*\\.png$|.*\\.jpg$|.*\\.jpeg$|.*\\.svg$).*)",
+  ],
   // what does the regex do?: https://rubular.com/r/aS10Q8RWoeBg54
 };
