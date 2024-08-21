b5f4f244b05fd8faf20c364e8ade5ceb79a2a64d
Nighel123 <nickel.paulsen@gmail.com>
2024-08-21 08:48:48 +0200

    further optimized pictures and fixed the gif bug

--- middleware.ts
+++ middleware.ts
@@ -44,7 +44,7 @@ export default NextAuth(authConfig).auth;
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: [
-    "/((?!api|_next/static|_next/image|.*\\.png$|.*\\.jpg$|.*\\.jpeg$|.*\\.svg$).*)",
+    "/((?!api|_next/static|_next/image|.*\\.png$|.*\\.jpg$|.*\\.jpeg$|.*\\.svg$|.*\\.gif$).*)",
   ],
   // what does the regex do?: https://rubular.com/r/aS10Q8RWoeBg54
 };
