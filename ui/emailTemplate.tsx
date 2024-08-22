df35c950b28695440e5f2316e61781526fad098a
Nighel123 <nickel.paulsen@gmail.com>
2024-08-23 01:05:48 +0200

    second scetch

--- ui/emailTemplate.tsx
+++ ui/emailTemplate.tsx
@@ -43,3 +43,30 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
     />
   </div>
 );
+
+export const NewMessagesTemplate = ({
+  recipientName,
+  senderName,
+  text,
+}: {
+  recipientName: string;
+  text: string;
+  senderName: string;
+}) => (
+  <div>
+    <h1>Hello, {recipientName}!</h1>
+    <p>You have new messages from {senderName}:</p>
+    <p>{text}</p>
+    <p>
+      Click <a href="www.roadtrip-germany.de">www.roadtrip-germany.de</a> to and
+      log in to answer {senderName}.
+    </p>
+    <Img
+      src="https://www.roadtrip-germany.de/routesOverview/loadingmap.gif"
+      width={555}
+      height={360}
+      alt="loading map"
+      id="mapSkeleton"
+    />
+  </div>
+);
