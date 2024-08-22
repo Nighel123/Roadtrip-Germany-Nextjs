bbade54defa5607daab895c4fac5e986938c9212
Nighel123 <nickel.paulsen@gmail.com>
2024-08-23 01:20:54 +0200

    before switching back branches

--- app/api/cron/newMessages/route.ts
+++ app/api/cron/newMessages/route.ts
@@ -14,5 +14,6 @@ export async function GET() {
   emailArray.forEach((o) => {
     sendNewMessagesEmail(o);
   });
-  await setMessagesToInformed();
+  //await setMessagesToInformed();
+  return;
 }
