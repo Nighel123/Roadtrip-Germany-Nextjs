48d9a6bdf3d3802ba62bdfd2bd6f4318c3ee9718
Nighel123 <nickel.paulsen@gmail.com>
2024-08-22 11:33:52 +0200

    accedentally deleted the images in the middleware

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ export default NextAuth(authConfig).auth;
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ["/((?!api|_next/static|_next/image).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|images).*)"],
   // what does the regex do?: https://rubular.com/r/aS10Q8RWoeBg54
 };
 /*   matcher: [
