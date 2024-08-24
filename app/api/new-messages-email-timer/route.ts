e650f407b550e816bbc0f321f08e8584baf3375b
Nighel123 <nickel.paulsen@gmail.com>
2024-08-24 12:52:36 +0200

    email blocked but I think at least it should work when I am unblocked

--- app/api/new-messages-email-timer/route.ts
+++ app/api/new-messages-email-timer/route.ts
@@ -2,7 +2,7 @@ import { sendNewMessagesEmails } from "lib/mail";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, res: NextResponse) {
-  sendNewMessagesEmails(20);
+  sendNewMessagesEmails(0);
   return new NextResponse("OK", {
     status: 200,
   });
