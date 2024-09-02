0b571db4c55faf18267c35c3d57603b9abb44c1a
Nighel123 <nickel.paulsen@gmail.com>
2024-09-02 17:19:07 +0200

    newMessage email cron job - error fixed when setting user to bee informed. Coloum 'too' did not exist. It was coloum 'to' that exists on table messages

--- app/api/cron/new-messages-email/route.ts
+++ app/api/cron/new-messages-email/route.ts
@@ -2,7 +2,11 @@ import { sendNewMessagesEmails } from "./newMessages";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, res: NextResponse) {
-  const promArr = await sendNewMessagesEmails();
-  const values = await Promise.all(promArr);
-  return NextResponse.json(values, { status: 200 });
+  try {
+    const promArr = await sendNewMessagesEmails();
+    const values = await Promise.all(promArr);
+    return NextResponse.json(values, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(error, { status: 200 });
+  }
 }
